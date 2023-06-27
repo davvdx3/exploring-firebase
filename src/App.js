67a6af637138bc87dc027487e2c06879cd060c86
David Aniel <davvdx3@gmail.com>
2023-06-27 08:36:54 +0000

    this is my second commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>firebase tutorial</h1>
+      <p>This is is my first firebase project</p>
+    </div>
+  );
+}
